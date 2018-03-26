166bb99832f8842229dd90093445b722007a073c
leon <leon@qeexo.com>
2018-03-26 14:21:29 +0800

    Update opencv version to 3.4.1

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 OPENCV_CAMERA_MODULES := on
 OPENCV_INSTALL_MODULES := on
-OPENCV_ANDROID_SDK := /Users/lvliang/Tools/OpenCV-android-sdk
+OPENCV_ANDROID_SDK := /Users/lvliang/Tools/OpenCV-android-sdk341
 include $(OPENCV_ANDROID_SDK)/sdk/native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES  := $(LOCAL_PATH)/com_example_lvliang_helloopencv_OpenCVClass.cpp
